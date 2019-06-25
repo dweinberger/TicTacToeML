f7cfaafa0e43da684bb9f8fa87631c0123198423
David Weinberger <david@weinberger.org>
2019-06-25 15:20:45 -0400

    Add json creation. Also, it works.

--- main.js
+++ main.js
@@ -39,7 +39,8 @@ games: array of all games
 
 
 var gNumberOfGamesToPlay = 100;
-
+var gAllTheGamesArray = [];
+var gFirstMover = -1;
 
 var gDisplayWinner = false; // show winning row
 var gShowStatus = true;
@@ -50,7 +51,7 @@ var gplayer;
 var gTotal1wins = 0;
 var gTotal0wins = 0;
 var gTotalTies =0;
-var gTotalGames = 0;
+var gGameCtr = 0;
 var gGames = [];
 var gLoadedGames = []; // games loaded from saves
 //var gutil = new Array();
@@ -64,12 +65,24 @@ function boxstruct(winner,moveNumber,owner) {
 
 var boxes = new Array();
 
-function moveStruct(player, box, boardstr,moveNumber,winner){
+function moveStruct(player, box, boardStr, nextBoardStr, moveNumber,winner, firstMover, movesUntilEnd, nextMove){
 	this.player = player;
 	this.box = box;
-	this.boardstr = boardstr;
+	this.boardStr = boardStr;
+	this.nextBoardStr = nextBoardStr;
 	this.moveNumber = moveNumber;
 	this.winner = winner;
+	this.firstMover = this.firstMover;
+	this.movesUntilEnd = this.movesUntilEnd;
+	this.nextMove=this.nextMove;
+}
+
+function gameStruct(index, winner, firstMover, numberOfMoves, moves){
+	this.index = index;
+	this.winner = winner;
+	this.firstMover = firstMover;
+	this.numberOfMoves = numberOfMoves;
+	this.moves = moves;
 }
 
 // the eight unique rows in the square
@@ -107,10 +120,10 @@ function init(){
 	gMoveNumber = 0;
 	gTotalXwins = 0;
 	gTotalOwins = 0;
-	gTotalGames = 0;
+	gGameCtr = 0;
 	gNoWinner = 0
 	// 	$("#status").text("");
-	gAllTheGamesArray = [];
+
 	
 	// global array of boxes
 	for (i=0; i < 9; i++){
@@ -130,6 +143,7 @@ function playGames(){
 	// -- Reset everything
 	
 	status("Resetting to play new round of games");
+	gAllTheGamesArray = [];
 	
 	gTotal1wins = 0;
 	gTotal0wins = 0;
@@ -144,11 +158,15 @@ function playGames(){
 			boxes[i].moveNumber = 0;
 			boxes[i].winner = -1;
 		}	
+		gFirstMover = player;
 
 	
 		// play a game
 		status("------- Starting Game # " + x + ". Player: " + player);
-		var winner = playGame(x, player);
+		var game = playGame(x, player);
+		winner = game.winner;
+		
+		// finished a game
 		if (winner == 0){
 			gTotal0wins++;
 		}
@@ -163,12 +181,19 @@ function playGames(){
 
 		status("0 wins: " + gTotal0wins + " 1 wins: " + gTotal1wins + " Ties: " + gTotalTies);
 		
-		// switch the player
+		// add game to the array
+		gAllTheGamesArray.push(game);
+		
+		gGameCtr++; // increment game counter
+		
+		// switch the first move player
 		player = (player === 0) ? 1 : 0;
 	}
 	
 	// ---- FINISHED PLAYING ALL GAMES
 	status("Finished playing " + (x + 1) + " games.");
+	createJson();
+	writeJson();
 }
 
 // -------- PLAY ONE GAME -------
@@ -181,7 +206,8 @@ function playGame(gameCtr, player){
 	var boxwinner =-1;
 	let stillPlaying = true;
 	var moveCtr = 0;
-	var game = []; // this will hold each move and other data
+	var game = new gameStruct;
+	game.moves = [];
 	// play the game
 	while (stillPlaying){
 		// set up for move
@@ -189,34 +215,67 @@ function playGame(gameCtr, player){
 		var move = new moveStruct;
 		move.player = player;
 		move.moveNumber = moveCtr;
+		// get a move, i.e., a random empty box
+		var boxToPlay = getAMove();
+		status("Got a move:" + boxToPlay);
+		
+		// if there is an available random move
+		if (boxToPlay != -1){
+			// mark the box as used
+			boxes[boxToPlay].owner = player;
+			move.boardStr = getboardString(boxToPlay);	
+			status(move.boardStr);
+		}
+		// if there are no moves left
+		else{
+			stillPlaying = false;
+		}
+		// did someone just win?
+		winner = checkForWinner();
+		if (winner > -1){
+			boxes[boxToPlay].winner = player;
+			stillPlaying = false;
+		}
+		
+		// finished a move. Record it.
+		//    (already recorded boardStr)
+		move.firstMover = gFirstMover;
+		move.player = player;
+		move.box = boxToPlay;
+		move.moveNumber = moveCtr;
+		move.boardStr 
+		
+		// add 
+		game.moves.push(move);
+		
+		// increment move counter
 		moveCtr++;
-		//while (done == false){
-			// get a move, i.e., a random empty box
-			var boxToPlay = getAMove();
-			status("Got a move:" + boxToPlay);
-			
-			// if there is an available random move
-			if (boxToPlay != -1){
-				// mark the box as used
-				boxes[boxToPlay].owner = player;
-				move.boardStr = getBoardString(boxToPlay);	
-				status(move.boardStr);
-			}
-			// if there are no moves left
-			else{
-				stillPlaying = false;
-			}
-			// did someone just win?
-			winner = checkForWinner();
-			if (winner > -1){
-				boxes[boxToPlay].winner = player;
-				stillPlaying = false;
-			}
-			game.push(move);
+		
+				
+		// switch the player
+		player = (player === 0) ? 1 : 0;
 	}
 	// game is over
-	gGames.push(game);
-	return winner;
+	
+	// record game (already recorded the moves)
+	game.index = gGameCtr ;
+	game.winner = winner;
+	game.firstMover = gFirstMover;
+	game.numberOfMoves = moveCtr;
+	
+	// modify moves array to get next board, etc.
+	var nextbrd = "";
+	for (var b=0; b < moveCtr - 1; b++){
+		var nbs = game.moves[b + 1].boardStr;
+		game.moves[b].nextBoardStr = game.moves[b + 1].boardStr;
+		game.moves[b].movesUntilEnd = game.numberOfMoves - b;
+		game.moves[b].nextMove = game.moves[b + 1].box;
+	}
+	// null out the last label
+	game.moves[moveCtr - 1].nextBoardStr = "";
+	game.moves[moveCtr - 1].nextMove = -1;
+		
+	return game;
   }
 
 function getAMove(){
@@ -244,7 +303,25 @@ function getAMove(){
 	return boxToPlay;
 }
 
-function getBoardString(box){
+function createJson(){
+	// creates json from the games global
+	
+	
+	
+}
+
+function writeJson(){
+	// writes out the json
+	var stringified = [];
+	for (var i=0; i < gAllTheGamesArray.length; i++){
+		var jsn = JSON.stringify(gAllTheGamesArray[i]);
+		stringified.push(jsn)
+	}
+	
+	return stringified;
+}
+
+function getboardString(box){
 	// go through the boxes constructing a string,
 	// one char for each box
 	var str = "";
